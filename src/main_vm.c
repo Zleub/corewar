07d3e6085bde65ad6ed4d09b31d27dc83ef75aa8
Zleub <debray.arnaud@gmail.com>
2015-01-22 06:28:11 +0100

    unclean commit

--- src/main_vm.c
+++ src/main_vm.c
@@ -16,7 +16,7 @@ void		ft_dump(void)
 	ft_printf("COMMENT_CMD_STRING: %s\n", COMMENT_CMD_STRING);
 	ft_printf("PROG_NAME_LENGTH: %d\n", PROG_NAME_LENGTH);
 	ft_printf("COMMENT_LENGTH: %d\n", COMMENT_LENGTH);
-	ft_printf("COREWAR_EXEC_MAGIC: %d\n", COREWAR_EXEC_MAGIC);
+	ft_printf("COREWAR_EXEC_MAGIC: %x\n", COREWAR_EXEC_MAGIC);
 	ft_printf("\nVM: \n");
 	ft_printf("VM per PROCESS: \n");
 	ft_printf("CYCLE_TO_DIE: %d\n", CYCLE_TO_DIE);
@@ -57,10 +57,35 @@ int		ft_isfile(t_env *env)
 	return (-1);
 }
 
+void showbits(unsigned int x)
+	{
+		int i;
+        for(i=(sizeof(int)*8)-1; i>=0; i--) {
+			(x&(1<<i))?ft_putchar('1'):ft_putchar('0');
+        }
+		ft_printf("\n");
+	}
+
+int		invertbits(unsigned int x)
+{
+	unsigned int test = 0;
+
+	((char*)&test)[3] = ((char*)&x)[0];
+	((char*)&test)[2] = ((char*)&x)[1];
+	((char*)&test)[1] = ((char*)&x)[2];
+	((char*)&test)[0] = ((char*)&x)[3];
+	return (test);
+}
+
 int			main(int argc, char **argv)
 {
 	t_env	*env;
 	header_t	plaiz;
+
+	manage_memory(INIT);
+	manage_memory(NEW);
+	t_memory *memory = manage_memory(GET);
+	(void)memory;
 
 	env = manage_env(GET);
 	env->argc = argc;
@@ -74,41 +99,35 @@ int			main(int argc, char **argv)
 		ft_printf("opt: %s, arg: %s\n", env->opt, env->arg);
 		int fd = ft_isfile(env);
 		// char str[4 + PROG_NAME_LENGTH + 1 + 4 + COMMENT_LENGTH + 1];
-		read(fd, &plaiz, 4 + PROG_NAME_LENGTH + 1 + 4 + COMMENT_LENGTH + 1);
+		read(fd, &plaiz, sizeof(header_t));
 
-		// ft_printf("%d\n", plaiz.magic);
-		int i = 3;
-		while (i >= 0) {
-			ft_printf("%d, ", ((char*)(&(plaiz.magic)))[i]);
-			i -= 1;
-		}
-		ft_printf("\n");
+		plaiz.magic = invertbits(plaiz.magic);
+		ft_printf("%x\n", plaiz.magic);
 		ft_printf("%s\n", plaiz.prog_name);
-		// ft_printf("%d\n", plaiz.prog_size);
-		i = 0;
-		while (i < 4) {
-			ft_printf("%d, ", ((char*)(&(plaiz.prog_size)))[i]);
-			i += 1;
-		}
-		ft_printf("\n");
+		plaiz.prog_size = invertbits(plaiz.prog_size);
+		ft_printf("%d\n", plaiz.prog_size);
 		ft_printf("%s\n", plaiz.comment);
 		// write(1, str, 4 + PROG_NAME_LENGTH + 1 + 4 + COMMENT_LENGTH + 1);
 
 		// manage_file(PRINTBINARY);
 		// manage_file(66);
 		// (*(t_file**)manage_file(GET)) = NULL;
+
+
+
+
+		char str[plaiz.prog_size];
+		read(fd, &str, plaiz.prog_size);
+
+		unsigned int k;
+		for(k = 0; k < plaiz.prog_size;k++)
+			ft_printf("%x ", str[k]);
+		ft_printf("\n");
+
+
 		manage_env(NEXT);
 	}
 
-	ft_dump();
-	// 	manage_memory(INIT);
-	// manage_memory(NEW);
-
-	// t_memory *memory = manage_memory(GET);
-	// memory->memory[0] = 11;
-	// memory->memory[3] = 1;
-	// memory->memory[1024] = 12;
-
-	// manage_memory(PRINT);
+	manage_memory(PRINT);
 	return (0);
 }
