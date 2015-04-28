cbbdf739e9b957936ee4e0b5a298ec7063b4ad38
trapcodien <trapcodien@hotmail.fr>
2015-04-29 01:32:17 +0200

    remove temp before exit...

--- unit_tests/ut.sh
+++ unit_tests/ut.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function clean_vm_tests()
+{
+	rm vm_tests/out 2> /dev/null > /dev/null
+	rm vm_tests/out2 2> /dev/null > /dev/null
+	rm vm_tests/valgrind_dump 2> /dev/null > /dev/null
+}
+
 RED="\033[31m"
 GREEN="\033[32m"
 RESET="\033[0m"
@@ -28,39 +35,35 @@ ls corewar 2> /dev/null > /dev/null || { echo -e $RED [KO] Unable to find corewa
 
 if [ "$level" == "1" ]
 then
-	rm vm_tests/out 2> /dev/null > /dev/null
+	clean_vm_tests
 	echo -n "- Execute : ./corewar" $*
-	./corewar $* > vm_tests/out || { echo -e $RED [KO] $RESET; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
-	rm vm_tests/out 2> /dev/null
+	./corewar $* > vm_tests/out || { echo -e $RED [KO] $RESET; clean_vm_tests ; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
+	clean_vm_tests
 fi
 
 if [ "$level" == "2" ]
 then
-	rm vm_tests/out 2> /dev/null > /dev/null
-	rm vm_tests/out 2> /dev/null > /dev/null
+	clean_vm_tests
 	echo -n "- Execute : ./corewar" $*
-	./corewar $* > vm_tests/out || { echo -e $RED [KO] $RESET; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
+	./corewar $* > vm_tests/out || { echo -e $RED [KO] $RESET; clean_vm_tests ; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
 	echo -n "- Second Execute : ./corewar" $*
-	./corewar $* > vm_tests/out2 || { echo -e $RED [KO] $RESET; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
+	./corewar $* > vm_tests/out2 || { echo -e $RED [KO] $RESET; clean_vm_tests exit 1; } && echo -e "$GREEN" [OK] "$RESET"
 	echo -n "- Diff"
-	diff vm_tests/out vm_tests/out2 >/dev/null 2> /dev/null || { echo -e $RED [KO] $RESET; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
-	rm vm_tests/out 2> /dev/null
-	rm vm_tests/out2 2> /dev/null
+	diff vm_tests/out vm_tests/out2 >/dev/null 2> /dev/null || { echo -e $RED [KO] $RESET; clean_vm_tests exit 1; } && echo -e "$GREEN" [OK] "$RESET"
+	clean_vm_tests
 fi
 
 if [ "$level" == "3" ]
 then
-	rm vm_tests/out 2> /dev/null > /dev/null
-	rm vm_tests/valgrind_dump 2> /dev/null > /dev/null
+	clean_vm_tests
 	echo -n "- Valgrind Execute : valgrind ./corewar" $*
-	valgrind ./corewar $* > vm_tests/out 2> vm_tests/valgrind_dump || { echo -e $RED [KO] $RESET; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
+	valgrind ./corewar $* > vm_tests/out 2> vm_tests/valgrind_dump || { echo -e $RED [KO] $RESET; clean_vm_tests ; exit 1; } && echo -e "$GREEN" [OK] "$RESET"
 	ERRORS=`cat vm_tests/valgrind_dump | tail -n 1 | cut -d ":" -f 2 | cut -d " " -f 2`
 	if [ $ERRORS != "0" ]
 	then
 		echo -e $RED $ERRORS [KOs] $RESET
 		exit 1
 	fi
-	rm vm_tests/out 2> /dev/null
-	rm vm_tests/valgrind_dump 2> /dev/null
+	clean_vm_tests
 fi
 
